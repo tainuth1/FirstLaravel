e47c4f2c54d2c8aae92dd735d3b77a598c344105
tainuth1 <tainuth1@gmail.com>
2024-07-27 19:59:25 +0700

    How to use raw SQL

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,29 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+
+    $users = DB::select('SELECT * FROM students');
+
+    // Insert data into database
+    // $user = DB::insert("INSERT INTO students (name, gender, age) VALUES(?, ?, ?)", [
+    //     'aaaa bbbb',
+    //     'ab',
+    //     200
+    // ]);
+
+    // Update data in database
+    // $user = DB::update("UPDATE students SET age = ? WHERE id = ?", [
+    //     19, 5
+    // ]);
+
+    //  Delete data from database
+    // $user = Db::delete("DELETE FROM students WHERE id = ?", [6]);
+
+    dd($users);
+
 });
 Route::get('/course', function () {
     return view('course');
