91e24878df12e65110aa0553a172baf8bceac23c
tainuth1 <tainuth1@gmail.com>
2024-07-29 20:37:52 +0700

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,68 +1,20 @@
 <?php
 
-use App\Models\Student;
-use App\Models\User;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
-    // Get data from database
-    // $users = DB::select('SELECT * FROM students');
-    // $users = DB::table('students')->get();
-    // $users = DB::table('students')->find(1);
-    // $users = DB::table('students')->get()->first();
-    // $users = DB::table('students')->get()->first();
-    // $users = Student::get()->where('gender', 'Male');
-    $users = User::find(4);
-
-    // Insert data into database
-    // $user = DB::insert("INSERT INTO students (name, gender, age) VALUES(?, ?, ?)", [
-    //     'aaaa bbbb',
-    //     'ab',
-    //     200
-    // ]);
-    // $user = DB::table('students')->insert([
-    //     'name' => 'ABC',
-    //     'gender' => 'male',
-    //     'age' => 19
-    // ]);
-    // $user = Student::insert([
-    //     'name' => 'Donal Trump',
-    //     'gender' => 'Male',
-    //     'age' => 65
-    // ]);
-    // $user = User::create([
-    //     'name' => 'Tai Nuth',
-    //     'email' => 'tainuth1@gmail.com',
-    //     'password' => 'Password'
-    // ]);
-
-    // Update data in database
-    // $user = DB::update("UPDATE students SET age = ? WHERE id = ?", [
-    //     19, 5
-    // ]);
-    // $user = DB::table('students')->where('id', 7)->update([
-    //     'name' => 'Mey Chethaputi'
-    // ]);
-    // $user = Student::where('id', 9)->update([
-    //     'name' => 'Neng Thida'
-    // ]);
-
-    //  Delete data from database
-    // $user = DB::delete("DELETE FROM students WHERE id = ?", [6]);
-    // $user = DB::table('students')->where('id', 8)->delete();
-    // $user = Student::where('id', 9)->delete();
-
-    dd($users->name);
-
+    return view('welcome');
 });
-Route::get('/course', function () {
-    return view('course');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
+
+require __DIR__.'/auth.php';
